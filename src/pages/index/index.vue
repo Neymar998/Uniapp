f71f052863d837e679ca40299ac179c0cb9ac474
winter <2064887138@qq.com>
2023-08-28 14:56:31 +0800

    chore:CustomNavBar

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-//
+import CustomNavbar from './components/CustomNavbar.vue'
 </script>
 
 <template>
+  <CustomNavbar />
   <view class="index">index</view>
 </template>
 
