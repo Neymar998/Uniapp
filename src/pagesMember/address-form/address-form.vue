f8dcd78b7f72f27584d092c3831ee8cd6e11214d
winter <2064887138@qq.com>
2023-09-10 19:16:34 +0800

    feat:form validation

--- src/pagesMember/address-form/address-form.vue
+++ src/pagesMember/address-form/address-form.vue
@@ -27,22 +27,49 @@ const onRegionChange: UniHelper.RegionPickerOnChange = (ev) => {
 const onSwitchChange: UniHelper.SwitchOnChange = (ev) => {
   form.value.isDefault = ev.detail.value ? 1 : 0
 }
+// 表单规则
+const rules: UniHelper.UniFormsRules = {
+  receiver: {
+    rules: [{ required: true, errorMessage: '请输入收货人姓名' }],
+  },
+  contact: {
+    rules: [
+      { required: true, errorMessage: '请输入收货人手机号码' },
+      { pattern: /^1[3-9]\d{9}$/, errorMessage: '请输入正确的手机号码' },
+    ],
+  },
+  fullLocation: {
+    rules: [{ required: true, errorMessage: '请选择省/市/区(县)' }],
+  },
+  address: {
+    rules: [{ required: true, errorMessage: '请输入详细地址' }],
+  },
+}
+const formRef = ref<UniHelper.UniFormsInstance>()
 // 提交表单
 const onSubmit = async () => {
-  if (query.id) {
-    // 修改地址
-    await putMemberAddressByIdAPI(query.id, form.value)
-  } else {
-    // 新建地址
-    await postMemberAddressAPI(form.value)
+  try {
+    await formRef.value?.validate?.()
+    if (query.id) {
+      // 修改地址
+      await putMemberAddressByIdAPI(query.id, form.value)
+    } else {
+      // 新建地址
+      await postMemberAddressAPI(form.value)
+    }
+    uni.showToast({
+      title: `${query.id ? '修改' : '新建'}地址成功`,
+      icon: 'success',
+    })
+    setTimeout(() => {
+      uni.navigateBack()
+    }, 500)
+  } catch (error) {
+    uni.showToast({
+      title: '请完善表单信息',
+      icon: 'error',
+    })
   }
-  uni.showToast({
-    title: `${query.id ? '修改' : '新建'}地址成功`,
-    icon: 'success',
-  })
-  setTimeout(() => {
-    uni.navigateBack()
-  }, 500)
 }
 // 获取路由id参数 确定是新建地址(没有id)还是修改地址
 const query = defineProps<{
@@ -65,25 +92,25 @@ uni.setNavigationBarTitle({ title: query.id ? '修改地址' : '新建地址' })
 
 <template>
   <view class="content">
-    <form>
+    <uni-forms :rules="rules" :model="form" ref="formRef">
       <!-- 表单内容 -->
-      <view class="form-item">
+      <uni-forms-item name="receiver" class="form-item">
         <text class="label">收货人</text>
         <input
           class="input"
           placeholder="请填写收货人姓名"
           v-model="form.receiver"
         />
-      </view>
-      <view class="form-item">
+      </uni-forms-item>
+      <uni-forms-item name="contact" class="form-item">
         <text class="label">手机号码</text>
         <input
           class="input"
           placeholder="请填写收货人手机号码"
           v-model="form.contact"
         />
-      </view>
-      <view class="form-item">
+      </uni-forms-item>
+      <uni-forms-item class="form-item" name="fullLocation">
         <text class="label">所在地区</text>
         <picker
           @change="onRegionChange"
@@ -94,15 +121,15 @@ uni.setNavigationBarTitle({ title: query.id ? '修改地址' : '新建地址' })
           <view v-if="form.fullLocation">{{ form.fullLocation }}</view>
           <view v-else class="placeholder">请选择省/市/区(县)</view>
         </picker>
-      </view>
-      <view class="form-item">
+      </uni-forms-item>
+      <uni-forms-item class="form-item" name="address">
         <text class="label">详细地址</text>
         <input
           class="input"
           placeholder="街道、楼牌号等信息"
           v-model="form.address"
         />
-      </view>
+      </uni-forms-item>
       <view class="form-item">
         <label class="label">设为默认地址</label>
         <switch
@@ -112,7 +139,7 @@ uni.setNavigationBarTitle({ title: query.id ? '修改地址' : '新建地址' })
           :checked="form.isDefault === 1"
         />
       </view>
-    </form>
+    </uni-forms>
   </view>
   <!-- 提交按钮 -->
   <button class="button" @tap="onSubmit">保存并使用</button>
