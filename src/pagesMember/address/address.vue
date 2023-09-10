c3d72491f25a7ed911bd79549abb785f718b60d7
winter <2064887138@qq.com>
2023-09-10 19:34:59 +0800

    feat:delete address

--- src/pagesMember/address/address.vue
+++ src/pagesMember/address/address.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
-import { getMemberAddressAPI } from '@/services/address'
+import {
+  getMemberAddressAPI,
+  deleteMemberAddressByIdAPI,
+} from '@/services/address'
 import { ref } from 'vue'
 import { onShow } from '@dcloudio/uni-app'
 import type { AddressItem } from '@/types/address'
@@ -9,7 +12,24 @@ const getMemberAddressData = async () => {
   const res = await getMemberAddressAPI()
   addressList.value = res.result
 }
-
+// 侧滑删除地址
+const onDeleteAddress = (id: string) => {
+  uni.showModal({
+    content: '删除地址？',
+    showCancel: true,
+    success: ({ confirm, cancel }) => {
+      if (confirm) {
+        deleteMemberAddressByIdAPI(id).then(() => {
+          uni.showToast({
+            title: '删除成功',
+            icon: 'success',
+          })
+          getMemberAddressData()
+        })
+      }
+    },
+  })
+}
 onShow(() => {
   getMemberAddressData()
 })
@@ -19,10 +39,14 @@ onShow(() => {
   <view class="viewport">
     <!-- 地址列表 -->
     <scroll-view class="scroll-view" scroll-y>
-      <view v-if="true" class="address">
-        <view class="address-list">
+      <view v-if="addressList.length" class="address">
+        <uni-swipe-action class="address-list">
           <!-- 收货地址项 -->
-          <view class="item" v-for="item in addressList" :key="item.id">
+          <uni-swipe-action-item
+            class="item"
+            v-for="item in addressList"
+            :key="item.id"
+          >
             <view class="item-content">
               <view class="user">
                 {{ item.receiver }}
@@ -40,8 +64,13 @@ onShow(() => {
                 修改
               </navigator>
             </view>
-          </view>
-        </view>
+            <template #right>
+              <button class="delete-button" @tap="onDeleteAddress(item.id)">
+                删除
+              </button>
+            </template>
+          </uni-swipe-action-item>
+        </uni-swipe-action>
       </view>
       <view v-else class="blank">暂无收货地址</view>
     </scroll-view>
--- src/services/address.ts
+++ src/services/address.ts
@@ -31,3 +31,10 @@ export const putMemberAddressByIdAPI = (id: string, data: AddressParams) => {
     data,
   })
 }
+// 删除收货地址
+export const deleteMemberAddressByIdAPI = (id: string) => {
+  return http({
+    method: 'DELETE',
+    url: `/member/address/${id}`,
+  })
+}
